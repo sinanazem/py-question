535e3d0a26666174020a33eea46ca019807d46ba
sinanazem <sinanazemm@gmail.com>
2023-11-25 03:23:08 +0330

    update

---
+++ linked-list/main.py
@@ -0,0 +1,24 @@
+list_ = [92, 97, 21, 79]
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, node):
+        self.head = None
+        
+first_node = Node(92)
+second_node = Node(97)
+third_node = Node(21)
+fourth_node = Node(79)
+
+first_node.next = second_node
+second_node.next = third_node
+third_node.next = fourth_node
+fourth_node.next = None
+
+ll = LinkedList(first_node)
+ll.head = first_node
+ll.head.data
