a790bb3d76d7d5b6023fb2d6f0ea79ff1799f8ca
sinanazem <sinanaazem@gmail.com>
2023-11-25 03:33:00 +0330

    update

--- linked-list/main.py
+++ linked-list/main.py
@@ -13,10 +13,3 @@ first_node = Node(92)
 second_node = Node(97)
 third_node = Node(21)
 fourth_node = Node(79)
-
-first_node.next = second_node
-second_node.next = third_node
-third_node.next = fourth_node
-fourth_node.next = None
-
-ll = LinkedList(first_node)
