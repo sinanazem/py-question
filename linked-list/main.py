f02bfac16066f032d4d96df6996138116206a756
Sina Nazem <92341812+sinanazem@users.noreply.github.com>
2023-11-27 03:37:21 +0330

    Update main.py

--- linked-list/main.py
+++ linked-list/main.py
@@ -1,15 +1,1 @@
-list_ = [92, 97, 21, 79]
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    def __init__(self, node):
-        self.head = None
-        
-first_node = Node(92)
-second_node = Node(97)
-third_node = Node(21)
-fourth_node = Node(79)
